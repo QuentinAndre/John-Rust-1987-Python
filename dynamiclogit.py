977df505e8025de68d40c2627c84ba3582f81923
QuentinAndre <quentin.andre@insead.edu>
2015-04-11 12:12:21 +0200

    Bug fixes + added functionalities
    .

--- dynamiclogit.py
+++ dynamiclogit.py
@@ -6,6 +6,9 @@ Created on Fri Apr 10 18:16:50 2015
 """
 import numpy as np
 import scipy.optimize as opt
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+from IPython.core.pylabtools import figsize
 
 class DynamicLogit(object):
     def __init__(self, data, Y, X, p, MF, npars):
@@ -44,29 +47,34 @@ class DynamicLogit(object):
                                          # state.
         
         
-        # Check that p is a correct vector of probabilities (i.e. sums to 1)        
+        # Check that p is a correct vector of probabilities (i.e. sums to 1)
+        p = np.array(p)        
         if p.sum() == 1:
             self.p = p
         else:
             raise ValueError(("The probability of state transitions should add" 
                               " up to 1!"))
-        self.MF = MF
-        self.npars = npars
+        
         
         # Check that the stated number of parameters correspond to the
         # specifications of the maintenance cost function.       
         try:
-            MF(1, [0]*npars)
+            MF(1, [0]*(npars-1))
         except ValueError:
             raise ValueError(("The number of parameters specified does not "
                               "match the specification of the maintenance cost"
                               " function!"))
+        else:
+            self.MF = MF
+            self.npars = npars
         
-        S = self.S        
+        
+              
         # To speed up computations and avoid loops when computing the log 
         # likelihood, we create a few useful matrices here:
+        S = self.S
         
-        # A (SxN) matrix indicating the state of each observation is created 
+        # A (SxN) matrix indicating the state of each observation
         self.state_mat = np.array([[self.exog[i]==s for i in range(self.N)] 
                                                     for s in range(self.S)])
         
@@ -87,15 +95,15 @@ class DynamicLogit(object):
         # certainty (used to compute the replacement utility)
         self.regen_mat = np.vstack((np.ones((1, S)),np.zeros((S-1, S))))
         
-        # A (2xN) matrix indicating with a dummy the decision taken for each
-        # time/bus 
+        # A (2xN) matrix indicating with a dummy the decision taken by the agent
+        # for each time/bus observation (replace or maintain)
         self.dec_mat = np.vstack(((1-self.endog), self.endog))
     
-        # A matrix d
     def myopic_costs(self, params):
         S = self.S
         """
-        This function computes the myopic expected cost associated with each decision for each state.
+        This function computes the myopic expected cost associated with each 
+        decision for each state.
         
         Takes:
             * A vector params, to be supplied to the maintenance cost function 
@@ -110,16 +118,6 @@ class DynamicLogit(object):
         maint_cost = [self.MF(s, thetas) for s in range(0, S)]
         repl_cost = [rc for state in range(0, S)]
         return np.vstack((maint_cost, repl_cost)).T
-    
-    def choice_prob(self, cost_array):
-        """
-        Returns the probability of each choice for each observed state, 
-        conditional on an array of state/decision costs.
-        """
-        cost = cost_array - cost_array.min(1).reshape(self.S, -1)
-        util = np.exp(-cost)
-        pchoice = util/(np.sum(util, 1).reshape(self.S, -1))
-        return pchoice
     
     def fl_costs(self, params, beta=0.75, threshold=1e-6, suppr_output=False):
         """
@@ -142,12 +140,12 @@ class DynamicLogit(object):
               sate and each decision.
         """
         achieved = True
-        # Initialization of CM
+        # Initialization of the contraction mapping
         k = 0
         EV = np.zeros((self.S, 2))
-        EV_myopic = EV_new = self.myopic_costs(params)
+        self.EV_myopic = EV_new = self.myopic_costs(params)
         
-        # CM Loop
+        # Contraction mapping Loop
         while abs(EV_new-EV).max() > threshold:
             EV = EV_new 
             pchoice = self.choice_prob(EV)
@@ -156,28 +154,67 @@ class DynamicLogit(object):
             futil_repl = np.dot(ecost, self.regen_mat)
             futil = np.vstack((futil_maint, futil_repl)).T
             
-            EV_new = EV_myopic + beta*futil
+            EV_new = self.EV_myopic + beta*futil
             k += 1
             if k == 1000:
                 achieved = False
                 break
 
+        # Output:
         if not suppr_output:
             if achieved:
                 print("Convergence achieved in {} iterations".format(k))
             else:
-                print("CM could not converge! Mean difference = {:.6f}".format((EV_new-EV).mean()))
+                print("CM could not converge! Mean difference = {:.6f}".format(
+                                                            (EV_new-EV).mean())
+                                                                              )
                 
-        self.EV = EV_new
+        self.EV_FL = EV_new
         return EV
+
+    def choice_prob(self, cost_array):
+        """
+        Returns the probability of each choice for each observed state, 
+        conditional on an array of state/decision costs (generated by the 
+        myopic or forward-looking cost functions)
+        """
+        cost = cost_array - cost_array.min(1).reshape(self.S, -1)
+        util = np.exp(-cost)
+        pchoice = util/(np.sum(util, 1).reshape(self.S, -1))
+        return pchoice
+    
+    def plot_gof(self):
+        """
+        Represent the goodness-of-fit of the forward-looking array of costs
+        obtained in maximum likelihood.
+        
+        This function plot the choice probabilities fitted to the data against 
+        the observed choice probabilities, and returns the plot.
+        """
+        obs_choice = pd.DataFrame({"Choice":self.endog, "State":self.exog})
+        obs_choice = obs_choice.groupby("State").mean().sort().reset_index()
+        plt.plot(self.choice_prob(self.EV_FL).T[0])
+        plt.plot(obs_choice["State"], 1-obs_choice["Choice"])
+        plt.ylabel("Probability of maintenance")
+        plt.xlabel("State of the bus")
+        plt.legend(["Fitted", "Observed"])
+        plt.xlim(0, self.S-1)
+        plt.ylim(0, 1.05)
+        plt.title("Action probabilities of the agent")
+        plt.show();
+        return None
         
     def loglike(self, params):
         """
         The log-likelihood of the Dynamic model is estimated in several steps.
-        1°) The currenter parameters are supplied to the contraction mapping function
-        2°) The function returns a matrix of decision probabilities for each state.
-        3°) This matrix is used to compute the loglikelihood of the observations
-        4°) The log-likelihood are then summed accross individuals, and returned
+        1°) The currenter parameters are supplied to the contraction mapping 
+            function
+        2°) The function returns a matrix of decision probabilities for each 
+            state.
+        3°) This matrix is used to compute the loglikelihood of the 
+            observations
+        4°) The log-likelihood are then summed accross individuals, and 
+            returned
         """
         util = self.fl_costs(params, suppr_output=True) 
         pchoice = self.choice_prob(util) 
@@ -220,13 +257,49 @@ class DynamicLogit(object):
           
 if __name__ == "__main__":
     import pandas as pd
+
+    # Loading data generated by a linear maintenance cost function  
+    print("Fitting a linear function to a linearly-generated dataset")  
+    datalin = pd.read_csv("Lin_Dataset.csv")    
     
+    # Linear maintenance cost function
     def lin_cost(s, params):
         theta1_1, = params
         return s*theta1_1
             
-    data = pd.read_csv("Lin_Dataset.csv")
+    # The state-transition probabilities
     p = np.array([0.36, 0.48, 0.16])
-    lin_to_lin = DynamicLogit(data, "Choice", "State", p, lin_cost, npars=3)
+    
+    # Initialization
+    lin_to_lin = DynamicLogit(datalin, "Choice", "State", p, lin_cost, npars=2)
+    
+    # Fitting the likelihood function
     lin_to_lin.fit_likelihood()
+    
+    # Printing parameters obtained
     lin_to_lin.print_parameters()
+    
+    # Generating GOF plot.
+    lin_to_lin.plot_gof()
+    
+    
+    # Loading data generated by an exponential maintenance cost function 
+    print("Fitting an exp. function to an exponentially-generated dataset")  
+    dataexp = pd.read_csv("Exp_Dataset.csv")    
+    
+    # Linear maintenance cost function
+    def exp_cost(s, params):
+        theta1_1, = params
+        return np.exp(s*theta1_1)
+    
+    # Initialization
+    exp_to_exp = DynamicLogit(dataexp, "Choice", "State", p, lin_cost, npars=2)
+    
+    # Fitting the likelihood function
+    exp_to_exp.fit_likelihood()
+    
+    # Printing parameters obtained
+    exp_to_exp.print_parameters()
+    
+    # Generating GOF plot.
+    exp_to_exp.plot_gof()
